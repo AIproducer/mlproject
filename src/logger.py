7ca761ab38eb3d24b123fe522c41d7a47a4060ca
20201009-091 <20201009-091@users.noreply.github.com>
2023-03-22 00:02:01 +0500

    Data trransofrmation done

--- src/logger.py
+++ src/logger.py
@@ -1,21 +1,21 @@
 import logging
+import sys
 import os
 from datetime import datetime
 
-LOG_FILE =f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
-
-log_path_PATH = os.path.join(logs_path,LOG_FILE)
-
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename=log_path_PATH,
-    format="[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 
 
-if __name__ =="__main__":
-    logging.info("Starting")
+       
+    
